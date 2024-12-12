fe2b37d293d2b3a21ad293822f292d8afcfeeea8
Broderjacob08 <alexander.hammarlund@elev.ga.lbs.se>
2024-12-12 19:36:45 +0100

    Fixade game over screen och victory screen + Guard speed buff

--- Assets/Final_boss_defeat.cs
+++ Assets/Final_boss_defeat.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Final_boss_defeat : MonoBehaviour
 {
+    [SerializeField]
+    int maxHealth = 1;
+
+    int health;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Hurt(int amount)
     {
-        
+        health -= amount;
+        if (health <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        SceneManager.LoadScene (2);
     }
 }
